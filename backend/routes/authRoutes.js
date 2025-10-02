3a6290207e3c1a9bc9c580dd157e7182933148e0
Chanchala Jeewanthi Jayalath <jeewanthichanchala321@gmail.com>
2025-10-02 17:18:00 +0530

    1

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { registerUser, loginUser, getUserProfile, updateUserProfile} = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddelware");
 
-const Router = express.Router();
+const router = express.Router();
 
 //Auth Routes
 router.post("/register", registerUser); //Register User
@@ -8,4 +10,12 @@ router.post("/login", loginUser); //Login User
 router.get("/profile", protect, getUserProfile); //Get User Profile
 router.put("/profile", protect, updateUserProfile); //Update User Profile
 
-module.exports = Router;
+router.post("/upload-image", uploas.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    res.status(200).json({ imageUrl});
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+//const userRoutes = require("./routes/userRoutes");
+//const taskRoutes = require("./routes/taskRoutes");
+//const reportROutes = require("./routes/reportRoutes");
+
 
 const app = express();
 //Middlewer to handle cors
@@ -24,10 +29,11 @@ app.use(express.json());
 
 
 //Routes
-//app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 //app.use("api/users", userRoutes);
 //app.use("api/tasks", taskRoutes); 
 //app.use("api/reports", reportROutes);
+
 
 
 
